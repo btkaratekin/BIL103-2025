1f654f821dd0b9159143198ad3f9bb5ed4cabf06
İsmail Karaman <ismailkaraman@msn.com>
2025-11-02 16:19:51 +0300

    son

---
+++ KararVerme/Ornek10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek10
+{
+    internal class Program
+    {
+        /* Örnek: Klavyeden girilen bir sayının tek yada çift olduğunu ekrana yazan algoritma ve programı oluşturunuz.
+         * 
+         * 
+         * Algoritma
+         * READ Sayi
+         * IF (Sayi % 2==0) THEN
+         *  WRITE "Sayi Çifttir"
+         * ELSE
+         *  WRITE "Sayi Tektir"
+         * END IF
+         */
+        static void Main(string[] args)
+        {
+            //Not: Bu örnek daha önce de if-else yapısı ile çözüldü. Burada ise ternary (?) operatörü kullanarak örnek çözümlenmektedir. 
+            Console.Title = "Tek Çift Sayı Bulma Uygulaması";
+            Console.Write("Bir Sayı Girin.....:");
+            int Sayi = int.Parse(Console.ReadLine());
+            string Sonuc = (Sayi % 2 == 0) ? "Sayı Çifttir" : "Sayı Tektir";
+            Console.WriteLine(Sonuc);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KararVerme/Ornek11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek11
+{
+    internal class Program
+    {
+        /*
+         * Örnek: Bir banka müşterisinin bankadan kredi alabilmesi için aşağıdaki şartları sağlaması gerekmektedir.
+         *      - Banka, müşteriye en fazla maaşının 36 katına kadar kredi verebilir.
+         *      - Müşteri devlet memuru değil ise bir kefil göstermek zorundadır. 
+         *      - Müşterinin devam eden bir kredi ödemesi var ise kredi verilemez.
+         *      - Müşteri bankadan daha önce kredi almış ise, ödeyeceği toplam borç üzerinden %10 indirim uygulanır.
+         *      
+         *      Bu kriterlere göre Klavyeden girilen Maaş tutarı, Devlet Memuru durumu, Kefil durumu, Devam eden kredi durumu ve Banka Müşterisi olup olmadığı bilgileri alınarak kişinin kredi alıp alamayacağını ve alabiliyorsa ne kadar kredi alabileceğini ekrana yazan algoritma ve programı oluşturunuz.
+         *      
+         * Algoritma
+         * READ Maas
+         * READ DevletMemuru
+         * IF (DevletMemuru==FALSE) THEN
+         *      READ KefilDurumu
+         * END IF
+         * READ DevamEdenKredi
+         * READ BankaMusterisi
+         * IF (DevamEdenKredi==true) THEN
+         *      IF
+         * 
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
